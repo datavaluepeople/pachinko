e3c93d0a73f5e5cbeb6ba96044cb7842706153ed
dbcerigo <dbcerigo@gmail.com>
2021-01-23 09:24:05 +0000

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="carrey",
+    packages=find_packages(),
+    version="0.0.a",
+    description="(Reinforcement learning) algorithms for optimising on cohorts of individuals.",
+    author="datavaluepeople",
+    license="",
+)
