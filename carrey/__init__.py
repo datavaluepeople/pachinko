1f530c8ab11a9df903c53892f73f9271c289eb9b
ali-tny <aliteeney@googlemail.com>
2021-01-23 21:57:05 +1100

    Add some baseline time period step agents
    - RandomAgent to choose a random action
    - EpsilonGreedy to choose the action with the highest conversion rate
      with some exploration

---
+++ carrey/__init__.py
@@ -0,0 +1,2 @@
+"""Init."""
+from carrey import time_period_step_agent  # noqa
---
+++ carrey/time_period_step_agent.py
@@ -0,0 +1,57 @@
+"""Agents interaction with time period step environments."""
+from typing import Any
+
+import numpy as np
+
+
+class RandomAgent:
+    """Agent taking a random action each timestep.
+
+    Args:
+        action_space: a list of the possible actions in the environment
+    """
+
+    def __init__(self, action_space: list):
+        self.action_space = action_space
+
+    def act(self, previous_observation: np.array) -> Any:
+        """Choose random action."""
+        return np.random.choice(self.action_space)
+
+
+class EpsilonGreedy:
+    """Agent greedily taking the highest conversion rate action (most of the time).
+
+    Args:
+        action_space: a list of the possible actions in the environment
+        epsilon: choose a random action `epsilon` percent of the time
+    """
+
+    def __init__(self, action_space: list, epsilon=0.05):
+        self.action_space = action_space
+        self.conversion_counts = {action: [0, 0] for action in action_space}
+        self.previous_action = None
+        self.epsilon = epsilon
+
+    def act(self, previous_observation: np.array) -> Any:
+        """Choose highest conversion rate action (with some exploration)."""
+        n_conversions = previous_observation[1]
+        if self.previous_action is not None:
+            self.conversion_counts[self.previous_action][0] += 1
+            self.conversion_counts[self.previous_action][1] += n_conversions
+
+        conversion_rate = {
+            # Set conversion rate to infinity for unchosen actions
+            # to ensure all actions are tested at least once
+            action: n_conv / n_chosen if n_chosen != 0 else np.inf
+            for action, (n_conv, n_chosen) in self.conversion_counts.items()
+        }
+        best_action = max(conversion_rate, key=conversion_rate.get)
+
+        if np.random.rand() < self.epsilon:
+            action = np.random.choice(self.action_space)
+        else:
+            action = best_action
+
+        self.previous_action = action
+        return action
